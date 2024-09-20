bd0cb7305cdd44194b1d11c6b44af752d0d6b630
nadaeezzdin <nadaeezzdin@gmail.com>
2024-09-20 21:27:58 +0300

    add swagger docs

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import { apiBaseRouter } from "./app/api/routes";
 import { errorHandler } from "./app/middlewares/errorHandler";
 import { notFoundHandler } from "./app/middlewares/notFoundHandler";
-//dotenv.config();
+import swaggerUi from 'swagger-ui-express';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,6 +21,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api", apiBaseRouter);
+
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, './utils/swagger.json'), 'utf-8')
+);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("**", notFoundHandler);
 
